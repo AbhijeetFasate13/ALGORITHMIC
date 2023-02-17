4e3729a7e78fd3bfe4c474aed4750fd3db48817a
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2023-02-17 17:51:24 +0530

    new variation

--- 1.cpp
+++ 1.cpp
@@ -1,3 +1,4 @@
 //https://codeforces.com/problemset/problem/617/A#:~:text=Problem%20%2D%20617A%20%2D%20Codeforces&text=An%20elephant%20decided%20to%20visit,4%20or%205%20positions%20forward.
 #include<iostream>
 using namesp
+fsdijfs
